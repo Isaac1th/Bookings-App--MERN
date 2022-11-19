86270bece5d2e398940190062ac280f57b522965
Isaac <you@example.com>
2022-11-19 17:10:48 +0200

    add route endpoints, connect mongoose to mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
+// Routes
 import mongoose from 'mongoose';
+import authRoute from './routes/auth.js';
+import usersRoute from './routes/users.js';
+import hotelsRoute from './routes/hotels.js';
+import roomsRoute from './routes/rooms.js';
+
 const app = express();
 dotenv.config();
 
@@ -17,9 +24,14 @@ mongoose.connection.on('disconnected', () => {
   console.log('mongoDB disconnected!');
 });
 
-mongoose.connection.on('connected', () => {
-  console.log('mongoDB connected!');
-});
+// middlewares
+
+app.use(express.json());
+
+app.use('/auth', authRoute);
+app.use('/users', usersRoute);
+app.use('/hotels', hotelsRoute);
+app.use('/rooms', roomsRoute);
 
 app.listen(3000, () => {
   connect();
